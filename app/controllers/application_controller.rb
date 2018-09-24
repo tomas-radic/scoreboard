d943c73b02c813fcfa28d660b81a401e9687b3b3
Tomas Radic <tomas.radic@gmail.com>
2018-09-24 19:54:48 +0200

    Added redirection to url with locale if missing, added menu item on static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,6 +44,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+    redirect_to root_path(locale: I18n.locale) if params[:locale].blank?
   end
 
   def self.default_url_options(options={})
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,3 @@
+I18n.load_path += Dir[Rails.root.join('config', 'locale', '*.{rb,yml}')]
 I18n.available_locales = [:en, :sk]
 I18n.default_locale = :en

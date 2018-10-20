6851c2b3b5f9b844c84b66980c99fc6c8916e6cc
Tomas Radic <tomas.radic@gmail.com>
2018-10-20 17:07:11 +0200

    Added courts switcher to the court show page

--- app/helpers/courts_helper.rb
+++ app/helpers/courts_helper.rb
@@ -35,4 +35,10 @@ module CourtsHelper
       result
     end
   end
+
+  def courts_switch_link_class(page_court, court)
+    result = 'nav-link btn btn-sm'
+    result += ' active btn-outline-success' if page_court.id == court.id
+    result
+  end
 end

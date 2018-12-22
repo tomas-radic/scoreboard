f816d2479f25a17e6cf9082093e5337b437f89e5
Tomas Radic <tomas.radic@gmail.com>
2018-12-22 16:50:19 +0100

    Automatic page reload when score has been updated

---
+++ db/migrate/20181221152212_add_score_update_key_to_courts.rb
@@ -0,0 +1,5 @@
+class AddScoreUpdateKeyToCourts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :courts, :public_key, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181027101227) do
+ActiveRecord::Schema.define(version: 20181221152212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20181027101227) do
     t.uuid "tournament_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "public_key"
     t.index ["tournament_id"], name: "index_courts_on_tournament_id"
   end
 

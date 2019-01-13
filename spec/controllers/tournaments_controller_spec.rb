c55b1ee331ddd2283e3d7c4bea352d028939b906
Tomas Radic <tomas.radic@gmail.com>
2019-01-13 15:11:00 +0100

    Added tournaments#index action test cases

--- spec/controllers/tournaments_controller_spec.rb
+++ spec/controllers/tournaments_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe TournamentsController, type: :controller do
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 
+RSpec.describe TournamentsController, type: :controller do
+  describe "GET index" do
+    subject { get :index }
+
+    context 'With signed in user' do
+      let!(:user) { FactoryBot.create(:user) }
+
+      before :each do
+        sign_in user
+      end
+
+      context 'And existing tournament of this user' do
+        let!(:tournament) { FactoryBot.create(:tournament, user: user) }
+
+        it 'Redirects to the existing tournament' do
+          expect(subject).to redirect_to action: :show, id: tournament.id
+        end
+      end
+
+      context 'And no existing tournament of this user' do
+        it 'Responds with status OK' do
+          expect(subject).to have_http_status(:ok)
+        end
+      end
+    end
+
+    context 'Without signed in user' do
+      it 'Redirects to sign in path' do
+        expect(subject).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
